261d897786afdffe442bb549cc15e0409656e409
조성수 <ski06043@naver.com>
2025-06-04 21:33:23 +0900

    Fix issues

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/chosungsu.github.io' : '',
   assetPrefix: '',
+  experimental: {
+    serverActions: false
+  }
 };
 
 module.exports = withMDX(nextConfig); 
